8d1474c9f4916e0e2dc9a2a60e2a36ea38f6cafb
Sparsh52 <chawlasparsh294@gmail.com>
2023-08-25 15:27:59 +0530

    edited

--- demo.py
+++ demo.py
@@ -1,3 +1,18 @@
-for i in range(10):
-    print("F1 Feature is added")
-    print("Hi")
+class Person:
+    def __init__(self,name,duty) -> None:
+        self.name=name
+        self.duty=duty
+    def printPerson(self):
+        print("Name is"+" "+self.name+" "+"duty is"+" "+self.duty)
+
+
+li=[]
+for i in range(5):
+    print("Enter the Name of Person")
+    name=input()
+    print("Enter The duty")
+    duty=input()
+    p=Person(name,duty)
+    li.append(p)
+for p in li:
+    p.printPerson()
